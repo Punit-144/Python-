a0fc0f5ffb837ff87a627037057f2fb1c4bba9ce
punit__22 <punitmathur10@gmail.com>
2025-01-16 16:17:26 +0530

    Updated the module.py file

--- Chapter 1/module.py
+++ Chapter 1/module.py
@@ -1,4 +1,9 @@
 import pyjokes
+# Print the Jokes
 
+# This Prints a random Jokes
 joke = pyjokes.get_joke()
 print(joke)
+
+
+# For multiline comment use the pair of tripple double inverted commas (""" """) to encode your comment 
