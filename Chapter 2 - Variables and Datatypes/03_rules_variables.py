5e3d727e427d443f3afe0bbdd42049134d6ab9d2
punit__22 <punitmathur10@gmail.com>
2025-01-16 17:22:20 +0530

    Uploaded the programs

---
+++ Chapter 2 - Variables and Datatypes/03_rules_variables.py
@@ -0,0 +1,13 @@
+# A variable name can contain alphabets, digits and underscore.
+# A variable name can only start with an aplhabet and underscore
+# A varianle name cannot start with a digit.
+# No white space is allowed to be used inside a variable name.
+
+a = 23 #valid
+
+punit = 07 #valid
+
+_punit = 22 #valid
+
+# @punit = 11 is invalid since we can only use alphabets underscore and digits
+# s@meer invalid due to @ symbol
