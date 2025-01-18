1b06413a0a0d1112f5cc6135786b597ff979c33e
punit__22 <punitmathur10@gmail.com>
2025-01-18 23:50:44 +0530

    Uploaded the First problem of pracice set

---
+++ Chapter 2 - Practice Set/Problem3.py
@@ -0,0 +1,6 @@
+# Check the type of variable assigned using input function
+
+a = input("Give the input to check the type of variable: ")
+print(type(a))
+
+# The output in this case will always be <class 'str'> since input function but this can be solved by typeconversion.
