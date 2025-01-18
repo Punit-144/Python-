522c93d7f8d0b0a3a3f10d424b82a98ef32957c4
punit__22 <punitmathur10@gmail.com>
2025-01-18 23:30:48 +0530

    Uploaded the program for this chapter

---
+++ Chapter 2 - Variables and Datatypes/06_Input.py
@@ -0,0 +1,12 @@
+a = input("Enter number 1: ")
+b = input("Enter number 2: ")
+print("Number a is: ", a)
+print("Number b is: ", b)
+print("Sum is: ",a + b) # Here the concatenation of the string will happen so for example a=1 and b=2 output will be '12'
+
+
+
+# To remove this problem we will typeconvert
+c = int(input("Enter number 1: "))
+d = int(input("Enter number 2: "))
+print("Sum is: ",c + d) # Here the concatenation of the string will happen so for example c=1 and d=2 output will be '3'
