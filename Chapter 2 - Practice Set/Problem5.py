34c9312e641e25b001950ba0e6675f92aa05264e
punit__22 <punitmathur10@gmail.com>
2025-01-19 00:16:43 +0530

    Uploaded the programs of pracice set

---
+++ Chapter 2 - Practice Set/Problem5.py
@@ -0,0 +1,6 @@
+# WAP to find an average of two numbers entered by the users.
+
+a = float(input("Enter the first number: "))
+b = float(input("Enter the second number: "))
+c = ((a + b)/2)
+print("Average of the two numbers is: ", c)
---
+++ Chapter 2 - Practice Set/Problem6.py
@@ -0,0 +1,5 @@
+# WAP to calculate the square of a number entered by user.
+
+a =float(input("Enter the number to get it's square : "))
+b = a*a
+print("The square of the number entered by user is: ", b)
