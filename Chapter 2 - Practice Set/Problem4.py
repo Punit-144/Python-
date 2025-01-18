841eb10a01c12e9ff5caddb5af88ed3faaa5451b
punit__22 <punitmathur10@gmail.com>
2025-01-18 23:56:53 +0530

    Uploaded the First problem of pracice set

---
+++ Chapter 2 - Practice Set/Problem4.py
@@ -0,0 +1,8 @@
+# Use Comparision operator to find out whether a given variable 'a' is greater tha 'b' or not. Take a = 34 and b = 80
+
+a = int(input("Enter a : "))
+b = int(input("Enter b : ")) 
+c = a > b
+print("Is a greater than b or not : ", c)
+
+# Question Specifically asked for fixed a and b values but i improvised to get input from users using typeconversion. 
