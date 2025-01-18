e034e1fe99adf40b39c6710a49342734951449b1
punit__22 <punitmathur10@gmail.com>
2025-01-18 23:08:46 +0530

    Uploaded the program for this chapter

---
+++ Chapter 2 - Variables and Datatypes/04_Operators.py
@@ -0,0 +1,28 @@
+# 1. Airthemetic Operators : +,-,*,/ etc.
+# 2. Assignment Operators : =,+=,-= etc.
+# 3. Comparision Operators : ==, >=, <, <=, >, != etc.
+# 4. Logical Operators : and, or, not.
+
+# Airthemetic Operators: 
+a= 34
+b = 45
+c = a + b
+print(c)
+
+# Assignment Operators:
+a = 4-2 # Assign 4-2 in a 
+print(a)
+b=6
+b += 3 # Increment the value of b by 3 and then assign it to b
+print(b)
+c=9
+c -= 3 # Dcrement the value of c by 3 and then assign it to c
+print(c)
+
+# Comparision Operators:
+d = 5<4
+print(d) # Output will be Falso (Always Boolean)
+
+# Logical Operators:
+e = 1 or 5
+print(e)
