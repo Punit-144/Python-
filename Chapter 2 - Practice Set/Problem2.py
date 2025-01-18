67952c510e969778598bac8f5fb2c97a7893b372
punit__22 <punitmathur10@gmail.com>
2025-01-18 23:42:07 +0530

    Uploaded the First problem of pracice set

---
+++ Chapter 2 - Practice Set/Problem2.py
@@ -0,0 +1,6 @@
+# WAP to find the remainder when a number is divided by z.
+
+a = int(input("Enter the number to get divided: "))
+b = int(input("Enter the number to divide from:  "))
+c = a % b
+print("The remainder of the a and b is: ", c)
