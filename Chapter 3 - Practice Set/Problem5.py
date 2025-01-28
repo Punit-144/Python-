4e2ef4d1254996cf83edbf9c3dabbbac383ccb79
Punit-144 <156175048+Punit-144@users.noreply.github.com>
2025-01-28 14:32:45 +0530

    Add files via upload

---
+++ Chapter 3 - Practice Set/Problem5.py
@@ -0,0 +1,5 @@
+# WAP to format the following letter using escape sequence characters.
+# Letter = "Dear Punit, this pythong course is nice. Thanks!"
+
+letter = "Dear Punit,\n\t This python course is nice. \nThanks!"
+print(letter)
