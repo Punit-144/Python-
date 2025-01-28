17c7b02a1533225316d79a9bf6a87bda2c89e9a5
Punit-144 <156175048+Punit-144@users.noreply.github.com>
2025-01-28 14:32:21 +0530

    Delete Chapter 3 - Practice Set/Problem5.py

--- Chapter 3 - Practice Set/Problem5.py
+++
@@ -1,5 +0,0 @@
-# WAP to format the following letter using escape sequence characters.
-# Letter = "Dear Punit, this pythong course is nice. Thanks!"
-
-letter = "Dear Punit,\n\t This python course is nice. \nThanks!"
-print(letter)
